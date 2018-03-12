b1081415cab22300d86e3dea43efcab7130667af
Stephanie <seal12514@gmail.com>
2018-03-12 01:35:19 +0000

    FINISHED!

--- js/script.js
+++ js/script.js
@@ -5,17 +5,17 @@
 // CREATE THE FUNCTIONS BELOW
 
 // Document Ready Function. All of your jQuery should go in here. 
-$( document ).ready(function() {
+$(document).ready(function() {
 	
 	function wordToPigLatin (wordy) {
-		for (var i = 0; i < word.length; i++){
-			if (startsWithAVowel(wordy)) {
-		    	return wordy + "ay"; 
+		for (var i = 0; i < wordy.length; i++){
+		    if (startsWithAVowel(wordy)) {
+		        return wordy + "ay"; 
 		    } else {
 		        return wordy.slice(1) + wordy[0] + "ay";
 		    }
+		}
 	}
-}
 	
 	function sentenceToPigLatin (sentence) {
 		for(var i = 0; i < sentence.length; i++){
@@ -24,29 +24,25 @@ $( document ).ready(function() {
 			for (var k = 0; k < words.length; k++) {
 			   var word = words[k];
 			   var pigLatinizedWord = wordToPigLatin(word);
-				pigLatinizedWords = pigLatinizedWords + pigLatinizedWord;
+			   pigLatinizedWords = pigLatinizedWords + " " + pigLatinizedWord;
 			}
 		}
 		return pigLatinizedWords;
 	}
 
-	function startsWithAVowel(word) {
-  var firstLetter = word[0].toLowerCase();
-  
-  if (firstLetter === "a" || firstLetter === "e" || firstLetter === "i" || firstLetter === "o" || firstLetter === "u") {
-    return true;
-  } else {
-    return false;
-  }
-}
+	function startsWithAVowel(wordo) {
+		var firstLetter = wordo[0].toLowerCase();
+		if (firstLetter === "a" || firstLetter === "e" || firstLetter === "i" || firstLetter === "o" || firstLetter === "u") {
+    	return true;
+		} else {
+    	return false;
+		}
+	}
 
-		$('#button').click(function(){
-		var sentency = $('#input').val();
-		var piglatin = sentenceToPigLatin(sentency);
+	$('#button').click(function(){
+		var sentencey = $('#textBox').val();
+		var piglatin = sentenceToPigLatin(sentencey);
 		$('#display').text(piglatin);
 	});
-	
-console.log(sentenceToPigLatin("Eat"));
-
 
 });
